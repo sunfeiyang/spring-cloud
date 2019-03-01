b4fe2066b183ec2385b136bd540d2ab69f1199b3
sunfeiyang <sfy5941@163.com>
2019-03-01 18:36:51 +0800

    add user modul

--- user/src/main/java/com/sunfy/UserApplication.java
+++ user/src/main/java/com/sunfy/UserApplication.java
@@ -1,13 +1,37 @@
 package com.sunfy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+// 成为一个服务
+@RestController
 public class UserApplication {
+
+    @Bean
+    // 访问SpringCloud的关键
+    RestTemplate restTemplate(){
+        return new RestTemplate() ;
+    }
+
+    @Autowired
+    // 自动装配对象
+    private RestTemplate restTemplate ;
+
+    @RequestMapping("/hi")
+    public String hi(@RequestParam(value="name" , defaultValue = "springcloud") String name){
+        String greeting = this.restTemplate.getForObject("http://localhost:5555/greeting",String.class) ;
+        // 返回值格式化
+        return String.format("%s , %s!" ,greeting , name) ;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
-
 }
