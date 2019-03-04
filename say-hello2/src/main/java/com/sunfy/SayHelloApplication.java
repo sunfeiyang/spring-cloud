6ea722d2b6ceb0e74ec4e30d2a0f2beb1b05f729
sunfeiyang <sfy5941@163.com>
2019-03-04 13:31:28 +0800

    Eureka服务中心获取列

---
+++ say-hello2/src/main/java/com/sunfy/SayHelloApplication.java
@@ -0,0 +1,41 @@
+package com.sunfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+// 添加RestController 修改为一个服务
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SayHelloApplication {
+
+    /**
+     * 问候语（模拟服务）
+     */
+    @RequestMapping("/greeting")
+    public String greet(){
+        List<String> greetings = Arrays.asList("hello1" ,"hello2" ,"hello3") ;
+        Random r = new Random() ;
+        // 每次调用时，随机返回字符串中的一个
+        String str = greetings.get(r.nextInt(3)) ;
+        System.out.println(str);
+        return str ;
+    }
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hi!!" ;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SayHelloApplication.class, args);
+    }
+}
